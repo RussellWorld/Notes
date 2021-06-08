5d0cbefd5f84ad2ce033f59ac7e510e2d59ab34b
RassellWorld <miraqq55@gmail.com>
2021-06-08 22:45:05 +0200

    Инициализация MainActivity

--- app/src/main/java/com/russellworld/notes/MainActivity.kt
+++ app/src/main/java/com/russellworld/notes/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.russellworld.notes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.russellworld.notes.databinding.ActivityMainBinding
+import com.russellworld.notes.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+    private var _binding: ActivityMainBinding? = null
+    val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        APP_ACTIVITY = this
+        mToolbar = mBinding.toolbar
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.title)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+
     }
 }
---
+++ app/src/main/java/com/russellworld/notes/utilits/constants.kt
@@ -0,0 +1,5 @@
+package com.russellworld.notes.utilits
+
+import com.russellworld.notes.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity
