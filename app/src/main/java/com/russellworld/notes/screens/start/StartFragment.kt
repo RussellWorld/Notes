e125e9615ed2e6253aa11f46b3c8c2d0ad2abf8b
RassellWorld <miraqq55@gmail.com>
2021-06-09 23:25:58 +0200

    Инициализация StartFragment и создание модели

---
+++ app/src/main/java/com/russellworld/notes/screens/start/StartFragment.kt
@@ -0,0 +1,38 @@
+package com.russellworld.notes.screens.start
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.russellworld.notes.databinding.FragmentStartBinding
+import com.russellworld.notes.utilits.TYPE_ROOM
+
+
+class StartFragment : Fragment() {
+
+    private var _binding: FragmentStartBinding? = null
+    private val mBinding get() = _binding!!
+    private lateinit var mViewModel: StartFragmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initialization()
+    }
+
+    private fun initialization() {
+        mViewModel = ViewModelProvider(this).get(StartFragmentViewModel::class.java)
+        mBinding.btnRoom.setOnClickListener {
+            mViewModel.initDatabase(TYPE_ROOM)
+        }
+    }
+}
--- app/src/main/java/com/russellworld/notes/utilits/constants.kt
+++ app/src/main/java/com/russellworld/notes/utilits/constants.kt
@@ -3,3 +3,5 @@ package com.russellworld.notes.utilits
 import com.russellworld.notes.MainActivity
 
 lateinit var APP_ACTIVITY: MainActivity
+const val TYPE_DATABASE = "type_database"
+const val TYPE_ROOM = "type_room"
