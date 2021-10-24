d130cda2cccda9417d89a611f466000ae5a95c25
RassellWorld <miraqq55@gmail.com>
2021-10-24 15:08:57 +0300

    Заполнение RecyclerView. Отображение списка заметок

--- app/src/main/java/com/russellworld/notes/screens/main/MainFragmentViewModel.kt
+++ app/src/main/java/com/russellworld/notes/screens/main/MainFragmentViewModel.kt
@@ -2,6 +2,8 @@ package com.russellworld.notes.screens.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.russellworld.notes.utilits.REPOSITORY
 
 class MainFragmentViewModel(application: Application): AndroidViewModel(application) {
+    val allNotes = REPOSITORY.allNotes
 }
